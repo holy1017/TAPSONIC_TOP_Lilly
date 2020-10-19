ecc0cf5fad1598da558632f97a91d3b906722947
holy <holy1017@naver.com>
2020-10-19 20:43:57 +0900

    노래 추가

--- TAPSONIC_TOP_Lilly/Form1.Designer.cs
+++ TAPSONIC_TOP_Lilly/Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.label0 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -49,7 +50,7 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(12, 12);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.Size = new System.Drawing.Size(296, 23);
             this.comboBox1.TabIndex = 0;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -58,7 +59,7 @@
             this.comboBox2.FormattingEnabled = true;
             this.comboBox2.Location = new System.Drawing.Point(12, 41);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 23);
+            this.comboBox2.Size = new System.Drawing.Size(296, 23);
             this.comboBox2.TabIndex = 0;
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
@@ -67,7 +68,7 @@
             this.comboBox3.FormattingEnabled = true;
             this.comboBox3.Location = new System.Drawing.Point(12, 70);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(121, 23);
+            this.comboBox3.Size = new System.Drawing.Size(296, 23);
             this.comboBox3.TabIndex = 0;
             this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
             // 
@@ -76,49 +77,49 @@
             this.comboBox4.FormattingEnabled = true;
             this.comboBox4.Location = new System.Drawing.Point(12, 99);
             this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(121, 23);
+            this.comboBox4.Size = new System.Drawing.Size(296, 23);
             this.comboBox4.TabIndex = 0;
             this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(139, 12);
+            this.label1.Location = new System.Drawing.Point(314, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(773, 23);
+            this.label1.Size = new System.Drawing.Size(598, 23);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(139, 41);
+            this.label2.Location = new System.Drawing.Point(314, 41);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(773, 23);
+            this.label2.Size = new System.Drawing.Size(598, 23);
             this.label2.TabIndex = 1;
             this.label2.Text = "label2";
             this.label2.Click += new System.EventHandler(this.label1_Click);
             // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(139, 70);
+            this.label3.Location = new System.Drawing.Point(314, 70);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(773, 23);
+            this.label3.Size = new System.Drawing.Size(598, 23);
             this.label3.TabIndex = 1;
             this.label3.Text = "label3";
             this.label3.Click += new System.EventHandler(this.label1_Click);
             // 
             // label4
             // 
-            this.label4.Location = new System.Drawing.Point(139, 99);
+            this.label4.Location = new System.Drawing.Point(314, 99);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(773, 23);
+            this.label4.Size = new System.Drawing.Size(598, 23);
             this.label4.TabIndex = 1;
             this.label4.Text = "label4";
             this.label4.Click += new System.EventHandler(this.label1_Click);
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(12, 154);
+            this.trackBar1.Location = new System.Drawing.Point(12, 157);
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(900, 45);
             this.trackBar1.TabIndex = 2;
@@ -126,16 +127,16 @@
             // 
             // label0
             // 
-            this.label0.Location = new System.Drawing.Point(12, 128);
+            this.label0.Location = new System.Drawing.Point(314, 128);
             this.label0.Name = "label0";
-            this.label0.Size = new System.Drawing.Size(121, 23);
+            this.label0.Size = new System.Drawing.Size(322, 23);
             this.label0.TabIndex = 1;
             this.label0.Text = "label0";
             this.label0.Click += new System.EventHandler(this.label1_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 205);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 208);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(900, 600);
             this.pictureBox1.TabIndex = 3;
@@ -144,18 +145,28 @@
             // 
             // label5
             // 
-            this.label5.Location = new System.Drawing.Point(139, 128);
+            this.label5.Location = new System.Drawing.Point(642, 128);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(121, 23);
+            this.label5.Size = new System.Drawing.Size(270, 23);
             this.label5.TabIndex = 1;
             this.label5.Text = "label5";
             this.label5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(12, 128);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(296, 23);
+            this.comboBox5.TabIndex = 0;
+            this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.comboBox5_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(928, 818);
+            this.ClientSize = new System.Drawing.Size(923, 818);
+            this.Controls.Add(this.comboBox5);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label0);
@@ -191,6 +202,7 @@
         private System.Windows.Forms.Label label0;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox5;
     }
 }
 
--- TAPSONIC_TOP_Lilly/Form1.cs
+++ TAPSONIC_TOP_Lilly/Form1.cs
@@ -28,6 +28,8 @@ namespace TAPSONIC_TOP_Lilly
         static List<Chra> cs;
         static List<Chra> cp;
 
+        static List<Song> so;
+
         static Font fnt = new Font("맑은 고딕", 16);
         //var fontf = new Font("맑은 고딕", 16, FontStyle.Regular, GraphicsUnit.Pixel);
         static Bitmap bitmap = new Bitmap(grp_w, grp_h);
@@ -80,7 +82,21 @@ namespace TAPSONIC_TOP_Lilly
             public override string ToString() => $"{name},\t {off},\t {on},\t {buff_v}";
         }
 
+        struct Song
+        {            
+            public string name;
+            public Att att;
+            public int time;
 
+            public Song(string name, Att att, int time)
+            {
+                this.name = name;
+                this.att = att;
+                this.time = time;
+            }
+
+            public override string ToString() => $"{name},\t {time}";
+        }
 
         public Form1()
         {
@@ -108,6 +124,7 @@ namespace TAPSONIC_TOP_Lilly
             ca.Add(new Chra("스이10", Att.vocal, Type.s, Buff.buff, 97, 10, 12));
             ca.Add(new Chra("슬래셔10", Att.vocal, Type.p, Buff.buff, 68, 13, 22));
             ca.Add(new Chra("걸윙10", Att.vocal, Type.p, Buff.combo, 100, 14, 10));
+            ca.Add(new Chra("보니10", Att.vocal, Type.s, Buff.combo, 100, 15, 8));
             ca.Add(new Chra("캐롤10", Att.dancer, Type.t, Buff.buff, 72, 12, 20));
             ca.Add(new Chra("셜리10", Att.dancer, Type.t, Buff.combo, 100, 17, 10));
             ca.Add(new Chra("데몬10", Att.dancer, Type.t, Buff.buff, 94, 17, 15));
@@ -122,7 +139,10 @@ namespace TAPSONIC_TOP_Lilly
             ca.Add(new Chra("크라켄10", Att.dancer, Type.l, Buff.combo, 100, 16, 10));
             ca.Add(new Chra("스페이시10", Att.dancer, Type.l, Buff.etc, 0, 0, 300));
             ca.Add(new Chra("아수라10", Att.dancer, Type.s, Buff.buff, 112, 12, 15));
-            ca.Add(new Chra("호련10", Att.dancer, Type.s, Buff.combo, 100, 15, 15));
+            ca.Add(new Chra("호련(교)10", Att.dancer, Type.s, Buff.combo, 100, 15, 15));
+            ca.Add(new Chra("호련(교)9", Att.dancer, Type.s, Buff.combo, 100, 15, 13));
+            ca.Add(new Chra("호련10", Att.dancer, Type.s, Buff.combo, 100, 15, 12));
+            ca.Add(new Chra("호련9", Att.dancer, Type.s, Buff.combo, 100, 15, 10));
             ca.Add(new Chra("스테파니10", Att.dancer, Type.s, Buff.etc, 7, 10, 24));
             ca.Add(new Chra("토미 슐츠10", Att.dancer, Type.s, Buff.buff, 89, 14, 14));
             ca.Add(new Chra("엘 페일10", Att.dancer, Type.s, Buff.buff, 93, 14, 15));
@@ -161,6 +181,249 @@ namespace TAPSONIC_TOP_Lilly
 
 
 
+            so = new List<Song>();
+
+            so.Add(new Song("A.I", Att.dancer, 144));
+            so.Add(new Song("Always", Att.dancer, 100));
+            so.Add(new Song("Angel", Att.dancer, 124));
+            so.Add(new Song("Angel (Hard, Expert)", Att.dancer, 127));
+            so.Add(new Song("Astral Supplication", Att.dancer, 121));
+            so.Add(new Song("Astro Fight", Att.dancer, 111));
+            so.Add(new Song("Bad Elixir", Att.dancer, 128));
+            so.Add(new Song("Beat U Down", Att.dancer, 91));
+            so.Add(new Song("Binary World", Att.dancer, 115));
+            so.Add(new Song("Black Water", Att.dancer, 140));
+            so.Add(new Song("Black Water (Hard, Expert)", Att.dancer, 141));
+            so.Add(new Song("Brain Power", Att.dancer, 110));
+            so.Add(new Song("Cherokee", Att.dancer, 125));
+            so.Add(new Song("Child of Night", Att.dancer, 105));
+            so.Add(new Song("confict", Att.dancer, 136));
+            so.Add(new Song("confict (Hard, Expert)", Att.dancer, 138));
+            so.Add(new Song("Cosmic Elevator", Att.dancer, 144));
+            so.Add(new Song("Cypher Gate", Att.dancer, 100));
+            so.Add(new Song("Delta Spike", Att.dancer, 115));
+            so.Add(new Song("Elastic Star", Att.dancer, 109));
+            so.Add(new Song("End Of Fight (Round 2)", Att.dancer, 131));
+            so.Add(new Song("End of the Moonlight", Att.dancer, 109));
+            so.Add(new Song("Fight Night", Att.dancer, 108));
+            so.Add(new Song("First Kiss", Att.dancer, 100));
+            so.Add(new Song("FTR", Att.dancer, 141));
+            so.Add(new Song("GAI∀PEX", Att.dancer, 116));
+            so.Add(new Song("Get Down", Att.dancer, 111));
+            so.Add(new Song("GET OUT", Att.dancer, 124));
+            so.Add(new Song("GET OUT ~Hip Noodle Mix~", Att.dancer, 137));
+            so.Add(new Song("Gient Step", Att.dancer, 141));
+            so.Add(new Song("Groovin'Up", Att.dancer, 122));
+            so.Add(new Song("Halcyon", Att.dancer, 155));
+            so.Add(new Song("HELIX", Att.dancer, 138));
+            so.Add(new Song("Here in the moment", Att.dancer, 95));
+            so.Add(new Song("Here in the moment (Hard, Expert)", Att.dancer, 100));
+            so.Add(new Song("HEXAD", Att.dancer, 94));
+            so.Add(new Song("Kamigakari", Att.dancer, 127));
+            so.Add(new Song("KILLER BEE", Att.dancer, 125));
+            so.Add(new Song("Liar", Att.dancer, 130));
+            so.Add(new Song("Light House", Att.dancer, 117));
+            so.Add(new Song("Long Vacation", Att.dancer, 127));
+            so.Add(new Song("Loving U", Att.dancer, 114));
+            so.Add(new Song("Mulch", Att.dancer, 107));
+            so.Add(new Song("Nature Fortress", Att.dancer, 114));
+            so.Add(new Song("NB RANGER", Att.dancer, 118));
+            so.Add(new Song("Nephthys", Att.dancer, 113));
+            so.Add(new Song("Over The Rainbow", Att.dancer, 110));
+            so.Add(new Song("Placebo Dying", Att.dancer, 149));
+            so.Add(new Song("Placebo Dying (Easy)", Att.dancer, 144));
+            so.Add(new Song("Ray of Illuminati", Att.dancer, 130));
+            so.Add(new Song("Ready Now", Att.dancer, 94));
+            so.Add(new Song("Revival On", Att.dancer, 147));
+            so.Add(new Song("RockSTAR", Att.dancer, 113));
+            so.Add(new Song("Shoreline", Att.dancer, 89));
+            so.Add(new Song("SigNalize", Att.dancer, 107));
+            so.Add(new Song("Silent Clarity", Att.dancer, 123));
+            so.Add(new Song("Sinister Evolution", Att.dancer, 122));
+            so.Add(new Song("Soda Pop City", Att.dancer, 115));
+            so.Add(new Song("SON OF SUN", Att.dancer, 102));
+            so.Add(new Song("SON OF SUN (Hard, Expert)", Att.dancer, 104));
+            so.Add(new Song("Space Corridor", Att.dancer, 102));
+            so.Add(new Song("Space of Soul", Att.dancer, 144));
+            so.Add(new Song("Splash Dreams", Att.dancer, 114));
+            so.Add(new Song("SQUEEZE", Att.dancer, 135));
+            so.Add(new Song("Stay with Me", Att.dancer, 121));
+            so.Add(new Song("STOP", Att.dancer, 124));
+            so.Add(new Song("Super Lovely", Att.dancer, 121));
+            so.Add(new Song("Tales of Princess Chocolate", Att.dancer, 140));
+            so.Add(new Song("Tales of Princess Chocolate (Easy)", Att.dancer, 136));
+            so.Add(new Song("The Feeling", Att.dancer, 123));
+            so.Add(new Song("The Sun Rising to Hope", Att.dancer, 118));
+            so.Add(new Song("The Sun Rising to Hope (Hard, Expert)", Att.dancer, 120));
+            so.Add(new Song("Thor", Att.dancer, 108));
+            so.Add(new Song("Tok! Tok! Tok!", Att.dancer, 134));
+            so.Add(new Song("U.A.D", Att.dancer, 122));
+            so.Add(new Song("Uranium", Att.dancer, 134));
+            so.Add(new Song("ViViT", Att.dancer, 125));
+            so.Add(new Song("VORTEX", Att.dancer, 122));
+            so.Add(new Song("VORTEX (Easy)", Att.dancer, 117));
+            so.Add(new Song("Watch Your Step", Att.dancer, 94));
+            so.Add(new Song("WhiteBlue", Att.dancer, 111));
+            so.Add(new Song("너랑 있으면", Att.dancer, 123));
+            so.Add(new Song("혜성(comet)", Att.dancer, 149));
+            so.Add(new Song("A Song of Sixpence", Att.vocal, 89));
+            so.Add(new Song("Across Starlight", Att.vocal, 144));
+            so.Add(new Song("ASHITA NO ASHIOTO", Att.vocal, 158));
+            so.Add(new Song("Ayatorite mau", Att.vocal, 125));
+            so.Add(new Song("Burn It Down", Att.vocal, 101));
+            so.Add(new Song("Bye Bye Love ~Nu Jazz Mix~", Att.vocal, 135));
+            so.Add(new Song("Colours of Sorrow", Att.vocal, 95));
+            so.Add(new Song("Cosmic Fantasy Lovesong", Att.vocal, 110));
+            so.Add(new Song("Deborah ", Att.vocal, 121));
+            so.Add(new Song("EGG", Att.vocal, 119));
+            so.Add(new Song("Eternal Memory ~ 소녀의 꿈~", Att.vocal, 129));
+            so.Add(new Song("Fallen Angel", Att.vocal, 117));
+            so.Add(new Song("Fallen Angel (Hard, Expert)", Att.vocal, 120));
+            so.Add(new Song("Fallin' In LUV", Att.vocal, 129));
+            so.Add(new Song("Feel", Att.vocal, 127));
+            so.Add(new Song("For the Ikarus", Att.vocal, 131));
+            so.Add(new Song("Give Me 5", Att.vocal, 116));
+            so.Add(new Song("GoodBye", Att.vocal, 113));
+            so.Add(new Song("Hello Pinky", Att.vocal, 128));
+            so.Add(new Song("I Want You", Att.vocal, 105));
+            so.Add(new Song("In My Heart", Att.vocal, 98));
+            so.Add(new Song("Jump to Summer", Att.vocal, 152));
+            so.Add(new Song("Ladymade Star", Att.vocal, 102));
+            so.Add(new Song("Leave me Alone", Att.vocal, 111));
+            so.Add(new Song("LENA Revolution", Att.vocal, 124));
+            so.Add(new Song("LET UP", Att.vocal, 131));
+            so.Add(new Song("Love☆Panic", Att.vocal, 127));
+            so.Add(new Song("Lover (BS Style)", Att.vocal, 86));
+            so.Add(new Song("Luv Flow", Att.vocal, 129));
+            so.Add(new Song("Luv is Ture", Att.vocal, 110));
+            so.Add(new Song("Mellow D Fantasy", Att.vocal, 133));
+            so.Add(new Song("Melody", Att.vocal, 99));
+            so.Add(new Song("Memoirs", Att.vocal, 131));
+            so.Add(new Song("My Jealousy", Att.vocal, 149));
+            so.Add(new Song("Never Say", Att.vocal, 122));
+            so.Add(new Song("One the Love", Att.vocal, 132));
+            so.Add(new Song("Only for you", Att.vocal, 136));
+            so.Add(new Song("Only for you (Hard, Expert)", Att.vocal, 139));
+            so.Add(new Song("Over The Horizon", Att.vocal, 145));
+            so.Add(new Song("Private Party", Att.vocal, 133));
+            so.Add(new Song("Raise Me Up", Att.vocal, 123));
+            so.Add(new Song("RED", Att.vocal, 123));
+            so.Add(new Song("Remember", Att.vocal, 105));
+            so.Add(new Song("Rhythm", Att.vocal, 114));
+            so.Add(new Song("Running girl", Att.vocal, 105));
+            so.Add(new Song("Saturday", Att.vocal, 132));
+            so.Add(new Song("Secret", Att.vocal, 110));
+            so.Add(new Song("Secret Dejavu", Att.vocal, 122));
+            so.Add(new Song("Shadow Flower", Att.vocal, 115));
+            so.Add(new Song("Showtime", Att.vocal, 123));
+            so.Add(new Song("Silver Bullet", Att.vocal, 145));
+            so.Add(new Song("sO mUCH iN LUV", Att.vocal, 130));
+            so.Add(new Song("Someday", Att.vocal, 106));
+            so.Add(new Song("Streetlight", Att.vocal, 119));
+            so.Add(new Song("Sunny Side", Att.vocal, 148));
+            so.Add(new Song("Sweet Shining Shooting Star", Att.vocal, 96));
+            so.Add(new Song("Take On Me", Att.vocal, 122));
+            so.Add(new Song("The Clear Blue Sky", Att.vocal, 87));
+            so.Add(new Song("Trip", Att.vocal, 111));
+            so.Add(new Song("Twinkle Star", Att.vocal, 111));
+            so.Add(new Song("U-NIVUS", Att.vocal, 129));
+            so.Add(new Song("Visual Dream II (In Fiction)", Att.vocal, 148));
+            so.Add(new Song("Wake Up feat.a-m", Att.vocal, 130));
+            so.Add(new Song("Wintermute", Att.vocal, 135));
+            so.Add(new Song("Y (CE STYLE)", Att.vocal, 101));
+            so.Add(new Song("You & Me", Att.vocal, 102));
+            so.Add(new Song("Your Own Miracle", Att.vocal, 77));
+            so.Add(new Song("너를 꿈꾸며", Att.vocal, 124));
+            so.Add(new Song("바람에게 부탁해", Att.vocal, 107));
+            so.Add(new Song("바람의 기억", Att.vocal, 146));
+            so.Add(new Song("별, 하늘, 그대", Att.vocal, 160));
+            so.Add(new Song("별, 하늘, 그대 (Hard, Expert)", Att.vocal, 164));
+            so.Add(new Song("별빛정원", Att.vocal, 114));
+            so.Add(new Song("비행", Att.vocal, 158));
+            so.Add(new Song("설레임", Att.vocal, 124));
+            so.Add(new Song("아침형 인간", Att.vocal, 134));
+            so.Add(new Song("유령", Att.vocal, 117));
+            so.Add(new Song("작은 꿈", Att.vocal, 154));
+            so.Add(new Song("징글벨", Att.vocal, 93));
+            so.Add(new Song("징글벨 (Hard, Expert)", Att.vocal, 98));
+            so.Add(new Song("Bingo", Att.session, 108));
+            so.Add(new Song("Black Swan", Att.session, 109));
+            so.Add(new Song("BlythE", Att.session, 129));
+            so.Add(new Song("Boom!", Att.session, 90));
+            so.Add(new Song("Brandnew Days", Att.session, 142));
+            so.Add(new Song("BUCK WILD", Att.session, 115));
+            so.Add(new Song("Can We Talk (Broken Dog Leg Mix)", Att.session, 136));
+            so.Add(new Song("CANNON 2017", Att.session, 148));
+            so.Add(new Song("D2", Att.session, 111));
+            so.Add(new Song("Dear My Lady", Att.session, 92));
+            so.Add(new Song("Devil's Whispering", Att.session, 135));
+            so.Add(new Song("DIVINE SERVICE", Att.session, 122));
+            so.Add(new Song("Don't Die", Att.session, 124));
+            so.Add(new Song("Don't Die (Hard, Expert)", Att.session, 126));
+            so.Add(new Song("Dream it", Att.session, 131));
+            so.Add(new Song("Driving Lazy Bee", Att.session, 89));
+            so.Add(new Song("Dual Strikers", Att.session, 106));
+            so.Add(new Song("Enemy Storm", Att.session, 122));
+            so.Add(new Song("Eternal Damnation", Att.session, 134));
+            so.Add(new Song("Fentanest", Att.session, 121));
+            so.Add(new Song("Festa Nova", Att.session, 121));
+            so.Add(new Song("Fly Away", Att.session, 128));
+            so.Add(new Song("Get On Top", Att.session, 126));
+            so.Add(new Song("Glory Day", Att.session, 131));
+            so.Add(new Song("Grid System", Att.session, 136));
+            so.Add(new Song("Haikai, Kingyobachi no sokokara", Att.session, 130));
+            so.Add(new Song("Heaven's vengeance", Att.session, 136));
+            so.Add(new Song("HELIOS", Att.session, 129));
+            so.Add(new Song("Higher", Att.session, 105));
+            so.Add(new Song("Humming Bird", Att.session, 117));
+            so.Add(new Song("Jibun kanpai", Att.session, 126));
+            so.Add(new Song("Keys to the World", Att.session, 125));
+            so.Add(new Song("Korobeiniki", Att.session, 112));
+            so.Add(new Song("La Campabella : Nu Rave", Att.session, 108));
+            so.Add(new Song("Lemonade", Att.session, 133));
+            so.Add(new Song("Mess it Up", Att.session, 129));
+            so.Add(new Song("Midnight Blood", Att.session, 137));
+            so.Add(new Song("Miles", Att.session, 124));
+            so.Add(new Song("Miles (Hard, Expert)", Att.session, 129));
+            so.Add(new Song("Mind Control", Att.session, 143));
+            so.Add(new Song("OBLIVION", Att.session, 118));
+            so.Add(new Song("OBLIVION (Easy)", Att.session, 117));
+            so.Add(new Song("Out Law", Att.session, 119));
+            so.Add(new Song("Persona", Att.session, 137));
+            so.Add(new Song("Rage of Demon", Att.session, 113));
+            so.Add(new Song("Rolling On the Duck", Att.session, 139));
+            so.Add(new Song("Runaway", Att.session, 124));
+            so.Add(new Song("Ruti'n", Att.session, 99));
+            so.Add(new Song("Ruti'n Electro Remix", Att.session, 115));
+            so.Add(new Song("Ruti'n Electro Remix (Easy)", Att.session, 113));
+            so.Add(new Song("SIN", Att.session, 114));
+            so.Add(new Song("StarFish", Att.session, 132));
+            so.Add(new Song("Sunset Rider", Att.session, 128));
+            so.Add(new Song("SuperSonic", Att.session, 104));
+            so.Add(new Song("The Party", Att.session, 125));
+            so.Add(new Song("Toys Festival", Att.session, 136));
+            so.Add(new Song("Urban Night", Att.session, 143));
+            so.Add(new Song("VERUM - oremus", Att.session, 133));
+            so.Add(new Song("Waiting for you", Att.session, 125));
+            so.Add(new Song("Weather Auction", Att.session, 113));
+            so.Add(new Song("Ya! Party!", Att.session, 97));
+            so.Add(new Song("고백, 꽃, 늑대 Part.2", Att.session, 95));
+            so.Add(new Song("고백, 꽃, 늑대 Part.2 (Hard, Expert)", Att.session, 108));
+            so.Add(new Song("바람에게 부탁해 Live Mix", Att.session, 104));
+            so.Add(new Song("태권부리", Att.session, 119));
+            so.Add(new Song("피아노 협주곡 1번", Att.session, 120));
+
+            // 정렬
+            ca.Sort(delegate (Chra x, Chra y)
+            {
+                return x.name.CompareTo(y.name);
+            });
+
+            // 정렬
+            so.Sort(delegate (Song x, Song y)
+            {
+                return x.name.CompareTo(y.name);
+            });
 
             ct = ca.Where(Chra => Chra.type == Type.t).ToList();
             cl = ca.Where(Chra => Chra.type == Type.l).ToList();
@@ -172,6 +435,10 @@ namespace TAPSONIC_TOP_Lilly
             comboBox2.Items.AddRange(cl.Select(Chra=> Chra.name).ToArray());
             comboBox3.Items.AddRange(cs.Select(Chra=> Chra.name).ToArray());
             comboBox4.Items.AddRange(cp.Select(Chra=> Chra.name).ToArray());
+
+
+
+            comboBox5.Items.AddRange(so.Select(Song=> Song.name).ToArray());
 
             trackBar1.Maximum = 180;
             trackBar1.Value = 120;
@@ -303,6 +570,16 @@ namespace TAPSONIC_TOP_Lilly
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox5.SelectedIndex == -1) return;
+            if (string.IsNullOrWhiteSpace(comboBox5.Text)) return;
+            time_song = so[comboBox5.SelectedIndex].time;
+            label0.Text = string.Format("{0:D2}:{1:D2}", time_song / 60, time_song % 60);
+            trackBar1.Value = time_song;
+            charBuffCalc();
         }
     }
 }
