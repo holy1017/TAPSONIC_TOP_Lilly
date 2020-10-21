66a92fb5aef98d1f1975dbd3ea3c89bc92c3ccf9
holy <holy1017@naver.com>
2020-10-21 23:25:48 +0900

    코드 최적화

--- TAPSONIC_TOP_Lilly/Form1.cs
+++ TAPSONIC_TOP_Lilly/Form1.cs
@@ -15,28 +15,31 @@ namespace TAPSONIC_TOP_Lilly
     {
         static int time_px = 5;// 초당 가로 픽셀
         static int time_song = 180;// 음악 최대 시간
-        static int grp_w = time_song* time_px;// 그래픽 크기
+        static int grp_w = time_song * time_px;// 그래픽 가로 크기
         static int grp_h_div = 2;//그래픽 배율 감소
-        static int grp_h = 300;// 그래픽 크기
-        static int[] buff_vall = Enumerable.Repeat(100, time_song).ToArray();
-        static int[] buff_v = Enumerable.Repeat(100, time_song).ToArray();
-        static bool[] buff_c = Enumerable.Repeat(false, time_song).ToArray();
+        static int grp_h = 300;// 그래픽 세로 크기
+        static int[] buff_vall = Enumerable.Repeat(0, time_song).ToArray();// 버프 합산
+        static int[] buff_v = Enumerable.Repeat(0, time_song).ToArray();//버프값. 만약 타입별로 한다면 이걸 분할해야함.
+        static bool[] buff_c = Enumerable.Repeat(false, time_song).ToArray();//콤보 유무
         static int sum = 0;// 그래픽 크기
 
-        static List<Chra> ca;
-        static List<Chra> ct;
-        static List<Chra> cl;
-        static List<Chra> cs;
-        static List<Chra> cp;
+        static List<Chra> ca;//모든캐릭
+        static List<Chra> ct;//탭캐릭. 분류 코드로 자동 처리
+        static List<Chra> cl;//탭캐릭. 분류 코드로 자동 처리
+        static List<Chra> cs;//탭캐릭. 분류 코드로 자동 처리
+        static List<Chra> cp;//탭캐릭. 분류 코드로 자동 처리
 
-        static List<Song> so;
+        static List<Song> so;//음악 목록
 
         static Font fnt = new Font("맑은 고딕", 16);
         //var fontf = new Font("맑은 고딕", 16, FontStyle.Regular, GraphicsUnit.Pixel);
         static Bitmap bitmap = new Bitmap(grp_w, grp_h);
         static Graphics graphics = Graphics.FromImage(bitmap);
 
-        enum Att    // 속성
+        /// <summary>
+        /// 속성. 댄서 세션 보컬
+        /// </summary>
+        enum Att
         {
             dancer,
             session,
@@ -44,7 +47,10 @@ namespace TAPSONIC_TOP_Lilly
             none
         }
 
-        enum Type    // 타입
+        /// <summary>
+        /// 타입. 탭 롱 슬라이드 플릿
+        /// </summary>
+        enum Type
         {
             tap,
             @long,
@@ -53,14 +59,15 @@ namespace TAPSONIC_TOP_Lilly
             none
         }
 
+        /// <summary>
+        /// 버프 종류. 만약 자버프 같은걸 넣을시 여기다가 해야할듯
+        /// </summary>
         enum Buff
         {
             combo,
             buff,
             etc
         }
-
-
 
         struct Chra
         {
@@ -80,14 +87,14 @@ namespace TAPSONIC_TOP_Lilly
                 this.buff_v = buff_v;
                 this.att = att;
                 this.off = off;
-                this.on = on ;
+                this.on = on;
             }
 
             public override string ToString() => $"{name},\t {type.ToString()},\t {att.ToString()},\t {buff.ToString()},\t {buff_v},\t {off},\t {on}";
         }
 
         struct Song
-        {            
+        {
             public string name;
             public Att att;
             public int time;
@@ -108,6 +115,44 @@ namespace TAPSONIC_TOP_Lilly
         {
             InitializeComponent();
 
+            setChra();
+            setSong();
+
+            trackBar1.Maximum = 180;
+            trackBar1.Value = 120;
+            trackBar1.LargeChange = 1;
+
+            // 콤보박스 세팅
+
+            comboBox1.DataSource = ct;
+            comboBox1.DisplayMember = "name";
+            comboBox1.SelectedIndex = 0;
+
+            comboBox2.DataSource = cl;
+            comboBox2.DisplayMember = "name";
+            comboBox2.SelectedIndex = 0;
+
+            comboBox3.DataSource = cs;
+            comboBox3.DisplayMember = "name";
+            comboBox3.SelectedIndex = 0;
+
+            comboBox4.DataSource = cp;
+            comboBox4.DisplayMember = "name";
+            comboBox4.SelectedIndex = 0;
+
+            comboBox5.DataSource = so;
+            comboBox5.DisplayMember = "name";
+            comboBox5.SelectedIndex = 0;
+
+            //pictureBox1.BackColor = Color.Black;
+
+            time_somgSet();
+
+            print();
+        }
+
+        private static void setChra()
+        {
             ca = new List<Chra>();
 
             // 캐릭 세팅            
@@ -191,8 +236,22 @@ namespace TAPSONIC_TOP_Lilly
             ca.Add(new Chra("에이미10", Att.session, Type.plit, Buff.etc, 0, 11, 1));
             ca.Add(new Chra("탈리아10", Att.session, Type.plit, Buff.etc, 0, 0, 300));
 
+            // 정렬
+            ca.Sort(delegate (Chra x, Chra y)
+            {
+                return x.name.CompareTo(y.name);
+            });
 
+            ct = ca.Where(Chra => Chra.type == Type.tap).ToList();
+            cl = ca.Where(Chra => Chra.type == Type.@long).ToList();
+            cs = ca.Where(Chra => Chra.type == Type.slide).ToList();
+            cp = ca.Where(Chra => Chra.type == Type.plit).ToList();
+        }
 
+        private static void setSong()
+        {
+
+            // 음악 세팅
             so = new List<Song>();
 
             so.Add(new Song(" ", Att.none, 180));
@@ -427,56 +486,86 @@ namespace TAPSONIC_TOP_Lilly
             so.Add(new Song("피아노 협주곡 1번", Att.session, 120));
 
             // 정렬
-            ca.Sort(delegate (Chra x, Chra y)
-            {
-                return x.name.CompareTo(y.name);
-            });
-
-            // 정렬
             so.Sort(delegate (Song x, Song y)
             {
                 return x.name.CompareTo(y.name);
             });
+        }
 
-            ct = ca.Where(Chra => Chra.type == Type.tap).ToList();
-            cl = ca.Where(Chra => Chra.type == Type.@long).ToList();
-            cs = ca.Where(Chra => Chra.type == Type.slide).ToList();
-            cp = ca.Where(Chra => Chra.type == Type.plit).ToList();
+        private void label1_Click(object sender, EventArgs e)
+        {
 
-            //comboBox2.Items.AddRange(cd2.Where(Chra => Chra.type == Type.t).Select(Chra=> Chra.name).ToArray());
-            //comboBox1.Items.AddRange(ct.Select(Chra=> Chra.name).ToArray());
-            //comboBox2.Items.AddRange(cl.Select(Chra => Chra.name).ToArray());
-            //comboBox3.Items.AddRange(cs.Select(Chra => Chra.name).ToArray());
-            //comboBox4.Items.AddRange(cp.Select(Chra => Chra.name).ToArray());
+        }
 
-            comboBox1.DataSource = ct;
-            comboBox1.DisplayMember = "name";
-            comboBox1.SelectedIndex = 0;
-            comboBox2.DataSource = cl;
-            comboBox2.DisplayMember = "name";
-            comboBox2.SelectedIndex = 0;
-            comboBox3.DataSource = cs;
-            comboBox3.DisplayMember = "name";
-            comboBox3.SelectedIndex = 0;
-            comboBox4.DataSource = cp;
-            comboBox4.DisplayMember = "name";
-            comboBox4.SelectedIndex = 0;
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            chraSelectedIndexChanged(sender, ct, label1);
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            chraSelectedIndexChanged(sender, cl, label2);
+        }
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            chraSelectedIndexChanged(sender, cs, label3);
+        }
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            chraSelectedIndexChanged(sender, cp, label4);
+        }
 
-            trackBar1.Maximum = 180;
-            trackBar1.Value = 120;
-            trackBar1.LargeChange = 1;
-
-            //comboBox5.Items.AddRange(so.Select(Song=> Song.name).ToArray());
-            comboBox5.DataSource = so;
-            comboBox5.DisplayMember = "name";
-            comboBox5.SelectedIndex = 0;
-
-
-
-            pictureBox1.BackColor = Color.Black;
-
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
             time_somgSet();
-            print();            
+            charBuffCalc();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (((ComboBox)sender).SelectedIndex == -1) return;
+            if (string.IsNullOrWhiteSpace(((ComboBox)sender).Text)) return;
+            time_song = ((Song)((ComboBox)sender).SelectedItem).time;
+            label0.Text = string.Format("{0:D2}:{1:D2}", time_song / 60, time_song % 60);
+            trackBar1.Value = time_song;
+            ((ComboBox)sender).BackColor = getColorAtt(((Song)((ComboBox)sender).SelectedItem).att);
+            charBuffCalc();
+
+        }
+
+        private void comboBox1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            boxColor<Chra>(sender, e);
+        }
+        private void comboBox2_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            boxColor<Chra>(sender, e);
+        }
+        private void comboBox3_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            boxColor<Chra>(sender, e);
+        }
+        private void comboBox4_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            boxColor<Chra>(sender, e);
+        }
+        private void comboBox5_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            boxColor<Song>(sender, e);
+        }
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_MeasureItem(object sender, MeasureItemEventArgs e)
+        {
+
         }
 
         /// <summary>
@@ -489,32 +578,24 @@ namespace TAPSONIC_TOP_Lilly
             buff_v = Enumerable.Repeat(0, time_song).ToArray();
             buff_c = Enumerable.Repeat(false, time_song).ToArray();
 
-            if (comboBox1.SelectedIndex >= 0)
-            {
-                //charBuffCalcSub(ct[comboBox1.SelectedIndex]);            
-                charBuffCalcSub((Chra)comboBox1.SelectedItem);            
-            }
-            if(comboBox2.SelectedIndex>=0)
-                charBuffCalcSub((Chra)comboBox2.SelectedItem);
-            if (comboBox3.SelectedIndex>=0)
-                charBuffCalcSub((Chra)comboBox3.SelectedItem);
-            if (comboBox4.SelectedIndex>=0)
-                charBuffCalcSub((Chra)comboBox4.SelectedItem);
+            charBuffCalcSub(comboBox1);            
+            charBuffCalcSub(comboBox2);            
+            charBuffCalcSub(comboBox3);            
+            charBuffCalcSub(comboBox4);            
 
             for (int i = 0; i < time_song; i++)
             {
                 if (buff_c[i])
                 {
-                    buff_vall[i] = (buff_v[i]+100) * 2;
+                    buff_vall[i] = (buff_v[i] + 100) * 2;
                 }
                 else
                 {
                     buff_vall[i] = buff_v[i] + 100;
                 }
-                //System.Diagnostics.Debug.WriteLine(buff_vall[i]);
                 sum += buff_vall[i];
             }
-            label5.Text= string.Format("{0}", sum);
+            label5.Text = string.Format("{0}", sum);
             print();
         }
 
@@ -523,13 +604,18 @@ namespace TAPSONIC_TOP_Lilly
         /// 만약 타입별 버프 필요하다면 여기서 수정 필요
         /// </summary>
         /// <param name="c"></param>
-        private static void charBuffCalcSub( Chra c)
+        private static void charBuffCalcSub(ComboBox comboBox)
         {
+            if (!(comboBox.SelectedIndex >= 0))
+                return;
+
+            Chra c=(Chra)comboBox.SelectedItem;
+
             if (c.buff == Buff.buff)
             {
                 for (int i = 0; i < time_song; i++)
                 {
-                    if (i % (c.on+c.off) > c.off)
+                    if (i % (c.on + c.off) > c.off)
                     {
                         buff_v[i] += c.buff_v;
                     }
@@ -554,40 +640,15 @@ namespace TAPSONIC_TOP_Lilly
         void print()
         {
             graphics.Clear(BackColor);
-            for (int i = grp_h-(99/ grp_h_div); i >=0 ; i-=(100/ grp_h_div))
+            for (int i = grp_h - (99 / grp_h_div); i >= 0; i -= (100 / grp_h_div))
             {
-                graphics.FillRectangle(Brushes.Black,0,i, time_song* time_px,1);
+                graphics.FillRectangle(Brushes.Black, 0, i, time_song * time_px, 1);
             }
             for (int i = 0; i < time_song; i++)
             {
-                graphics.FillRectangle(Brushes.Green,i* time_px, grp_h-(buff_vall[i]/ grp_h_div),time_px, buff_vall[i] / grp_h_div);
+                graphics.FillRectangle(Brushes.Green, i * time_px, grp_h - (buff_vall[i] / grp_h_div), time_px, buff_vall[i] / grp_h_div);
             }
             pictureBox1.Image = bitmap;
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            //Console.Write(comboBox1.SelectedIndex);
-            //Console.WriteLine(comboBox1.Text);
-            chraSelectedIndexChanged(sender, ct,label1);
-
-        }
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            chraSelectedIndexChanged(sender, cl,label2);
-        }
-        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            chraSelectedIndexChanged(sender, cs,label3);
-        }
-        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            chraSelectedIndexChanged(sender, cp,label4);
         }
 
         /// <summary>
@@ -615,39 +676,13 @@ namespace TAPSONIC_TOP_Lilly
             charBuffCalc();
         }
 
-        private void trackBar1_Scroll(object sender, EventArgs e)
-        {
-            time_somgSet();
-            charBuffCalc();
-        }
-
+        /// <summary>
+        /// 시간 라벨에 출력
+        /// </summary>
         private void time_somgSet()
         {
-            int t = trackBar1.Value;
             time_song = trackBar1.Value;
-            label0.Text = string.Format("{0:D2}:{1:D2}", t / 60, t % 60);
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (((ComboBox)sender).SelectedIndex == -1) return;
-            if (string.IsNullOrWhiteSpace(((ComboBox)sender).Text)) return;
-            time_song = ((Song)((ComboBox)sender).SelectedItem).time;
             label0.Text = string.Format("{0:D2}:{1:D2}", time_song / 60, time_song % 60);
-            trackBar1.Value = time_song;
-            ((ComboBox)sender).BackColor = getColorAtt(((Song)((ComboBox)sender).SelectedItem).att);
-            charBuffCalc();
-          
-        }
-
-        private void comboBox1_DrawItem(object sender, DrawItemEventArgs e)
-        {
-            boxColor<Chra>(sender, e);
         }
 
         /// <summary>
@@ -673,6 +708,7 @@ namespace TAPSONIC_TOP_Lilly
             e.Graphics.FillRectangle(getBrushAtt(att), e.Bounds);
             e.Graphics.DrawString(((ComboBox)sender).Items[e.Index].ToString(), ((Control)sender).Font, Brushes.Black, e.Bounds.X, e.Bounds.Y);
         }
+
 
         /// <summary>
         /// 속성별 색 얻기
@@ -725,38 +761,6 @@ namespace TAPSONIC_TOP_Lilly
                     return Color.Brown;
 
             }
-        }
-
-        private void comboBox2_DrawItem(object sender, DrawItemEventArgs e)
-        {
-            boxColor<Chra>(sender, e);
-        }
-
-        private void comboBox3_DrawItem(object sender, DrawItemEventArgs e)
-        {
-            boxColor<Chra>(sender, e);
-        }
-
-        private void comboBox4_DrawItem(object sender, DrawItemEventArgs e)
-        {
-            boxColor<Chra>(sender, e);
-        }
-
-
-        private void comboBox5_DrawItem(object sender, DrawItemEventArgs e)
-        {
-            boxColor<Song>(sender, e);
-        }
-
-        private void comboBox1_DropDown(object sender, EventArgs e)
-        {
-
-        }
-
-        private void comboBox1_MeasureItem(object sender, MeasureItemEventArgs e)
-        {
-            Console.WriteLine("comboBox1_MeasureItem");
-            //System.Diagnostics.Debug.WriteLine("comboBox1_MeasureItem");
         }
     }
 }
